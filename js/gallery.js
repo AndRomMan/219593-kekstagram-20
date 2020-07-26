ef92a758782aca2c578adab22e6c67d85ed62baf
AndRomMan <demromdeveloper@gmail.com>
2020-07-26 23:11:43 +0300

    style(js): fix some style errors

--- js/gallery.js
+++ js/gallery.js
@@ -14,40 +14,40 @@
     };
   }
 
-      function getUserPictureFragment(template, imgCollection) {
-      var userPicturesFragment = document.createDocumentFragment();
-      imgCollection.forEach(function (loadedImgObj) {
-        var templateClone = template.cloneNode(true);
-        setUserPictureAttributes(templateClone, loadedImgObj);
-        userPicturesFragment.append(templateClone);
-      });
-      return userPicturesFragment;
-    }
+  function getUserPictureFragment(template, imgCollection) {
+    var userPicturesFragment = document.createDocumentFragment();
+    imgCollection.forEach(function (loadedImgObj) {
+      var templateClone = template.cloneNode(true);
+      setUserPictureAttributes(templateClone, loadedImgObj);
+      userPicturesFragment.append(templateClone);
+    });
+    return userPicturesFragment;
+  }
 
-    function setUserPictureAttributes(element, loadedImgObj) {
-      var imgElement = element.querySelector('.picture__img');
-      var commentsElement = element.querySelector('.picture__comments');
-      var likesElement = element.querySelector('.picture__likes');
-      var imgUrl = loadedImgObj.url;
-      var imgComments = loadedImgObj.comments;
-      var imgLikes = loadedImgObj.likes;
+  function setUserPictureAttributes(element, loadedImgObj) {
+    var imgElement = element.querySelector('.picture__img');
+    var commentsElement = element.querySelector('.picture__comments');
+    var likesElement = element.querySelector('.picture__likes');
+    var imgUrl = loadedImgObj.url;
+    var imgComments = loadedImgObj.comments;
+    var imgLikes = loadedImgObj.likes;
 
-      imgElement.setAttribute('src', imgUrl);
-      commentsElement.textContent = (String(imgComments.length));
-      likesElement.textContent = (String(imgLikes));
-    }
+    imgElement.setAttribute('src', imgUrl);
+    commentsElement.textContent = (String(imgComments.length));
+    likesElement.textContent = (String(imgLikes));
+  }
 
-    function renderFilteredCollection(template, filteredPhotoCollection) {
-      clearGallery();
-      userGallery.append(getUserPictureFragment(template, filteredPhotoCollection));
-    }
+  function renderFilteredCollection(template, filteredPhotoCollection) {
+    clearGallery();
+    userGallery.append(getUserPictureFragment(template, filteredPhotoCollection));
+  }
 
-    function clearGallery() {
-      var galleryImgs = document.querySelectorAll('.' + window.bigPicture.LINK_CLASS);
-      galleryImgs.forEach(function (galleryImg) {
-        galleryImg.remove();
-      });
-    }
+  function clearGallery() {
+    var galleryImgs = document.querySelectorAll('.' + window.bigPicture.LINK_CLASS);
+    galleryImgs.forEach(function (galleryImg) {
+      galleryImg.remove();
+    });
+  }
 
   window.gallery = {
     renderGallery: renderGallery,
